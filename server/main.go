679cde50f3540de60745d7920fd5b92863dc87ee
kobayashi <citcho716@gmail.com>
2023-01-16 00:55:35 +0900

    feat: FileServiceServer

---
+++ server/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"grpc/pb"
+	"io/ioutil"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedFileServiceServer
+}
+
+func (*server) ListFiles(ctx context.Context, req *pb.ListFilesRequest) (*pb.ListFilesResponse, error) {
+	fmt.Println("ListFiles was invoked.")
+
+	dir := "/Users/kobayashi/Documents/kbys/src/grpc/storage"
+	paths, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var filenames []string
+	for _, path := range paths {
+		if !path.IsDir() {
+			filenames = append(filenames, path.Name())
+		}
+	}
+
+	res := &pb.ListFilesResponse{
+		Filenames: filenames,
+	}
+	return res, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "localhost:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterFileServiceServer(s, &server{})
+
+	fmt.Println("server is running...")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
