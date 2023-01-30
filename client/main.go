04c176eb130e72fedd5afce17bf002a424d0dbba
kobayashi <citcho716@gmail.com>
2023-01-30 23:09:43 +0900

    feat: server streaming rpc

--- client/main.go
+++ client/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"grpc/pb"
+	"io"
 	"log"
 
 	"google.golang.org/grpc"
@@ -18,7 +19,8 @@ func main() {
 
 	client := pb.NewFileServiceClient(conn)
 
-	callListFiles(client)
+	// callListFiles(client)
+	callDownload(client)
 }
 
 func callListFiles(client pb.FileServiceClient) {
@@ -28,4 +30,25 @@ func callListFiles(client pb.FileServiceClient) {
 	}
 
 	fmt.Println(res.GetFilenames())
+}
+
+func callDownload(client pb.FileServiceClient) {
+	req := &pb.DownloadRequest{Filename: "name.txt"}
+	stream, err := client.Download(context.Background(), req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Printf("Response from Download(bytes): %v", res.GetData())
+		log.Printf("Response from Download(string): %v", string(res.GetData()))
+	}
 }
--- server/main.go
+++ server/main.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"grpc/pb"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -35,6 +38,39 @@ func (*server) ListFiles(ctx context.Context, req *pb.ListFilesRequest) (*pb.Lis
 		Filenames: filenames,
 	}
 	return res, nil
+}
+
+func (*server) Download(req *pb.DownloadRequest, stream pb.FileService_DownloadServer) error {
+	fmt.Println("Download was invoked")
+
+	filename := req.GetFilename()
+	path := "/Users/kobayashi/Documents/kbys/src/grpc/storage/" + filename
+
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	buf := make([]byte, 5)
+	for {
+		n, err := file.Read(buf)
+		if n == 0 || err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		res := &pb.DownloadResponse{Data: buf[:n]}
+		sendErr := stream.Send(res)
+		if sendErr != nil {
+			return sendErr
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil
 }
 
 func main() {
